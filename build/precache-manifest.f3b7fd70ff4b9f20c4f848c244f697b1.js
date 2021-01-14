2583dac236f4a4f213d1b3c42a4244417b83a4c8
netolopes <netolopes27@gmail.com>
2021-01-14 17:27:40 -0300

    correcoes ...

---
+++ build/precache-manifest.f3b7fd70ff4b9f20c4f848c244f697b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f1dff27ecca336a044edc288e4ed077",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c133f2ad74d825f1579e",
+    "url": "/static/css/main.bb7ea331.chunk.css"
+  },
+  {
+    "revision": "7a2927fab3e753b1350a",
+    "url": "/static/js/2.f702b8dd.chunk.js"
+  },
+  {
+    "revision": "c133f2ad74d825f1579e",
+    "url": "/static/js/main.5671406e.chunk.js"
+  },
+  {
+    "revision": "fef09335e8b2a5b84166",
+    "url": "/static/js/runtime~main.6a467959.js"
+  },
+  {
+    "revision": "c82062c113a7bcdafb57478c2f367775",
+    "url": "/static/media/spinner.c82062c1.gif"
+  }
+]);
